74ab5579251a2f8ee54887ee156d220bc95d4134
gaaelle <gaaelle.robart@gmail.com>
2017-10-20 15:28:30 +0200

    une liste de films

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 
+import { Movie } from '../shared/movie.model';
+import { MovieService } from '../shared/movie.service';
+
+@Injectable()
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-
-  constructor() { }
+  movies: Movie[] = [];
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+    this.movieService.getMovies().subscribe(
+      data => this.movies = data,
+      err => console.log(err)
+    );
   }
 
 }
---
+++ src/app/shared/movie.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do'; //Pour visualiser les donéés
+
+import { Movie } from '../shared/movie.model';
+
+@Injectable()
+export class MovieService {
+  constructor(private http: Http) {}
+
+  getMovies() {
+    return this.http.get('http://localhost:3000/movies')
+    .do(data => console.log(data))
+    .map(movies => movies.json());
+  }
+}
