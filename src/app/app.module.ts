6b0d6ed25ca4247afd3f93e269bc1c9b97eac8e7
gaaelle <gaaelle.robart@gmail.com>
2017-10-20 11:22:13 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieFormComponent } from './movie-form/movie-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovieListComponent,
+    MovieFormComponent
   ],
   imports: [
     BrowserModule
